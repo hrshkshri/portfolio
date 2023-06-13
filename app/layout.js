ac35b383e8f1c59862aa47b399aa9c1de9ba4cc3
Harsh <harshkeshari100@gmail.com>
2023-06-13 19:35:38 +0530

    fixing font

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} mx-16`}>
+      <body className={`${inter.className} mx-16 bg-amber-100`}>
         <Navbar />
         <div>{children}</div>
         <Footer />
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import Image from "next/image";
 
 const Header = () => {
   return (
-    <div className="w-full h-full flex justify-between items-center mx-5 my-5 py-5 ">
-      <div className="flex flex-col space-y-2">
+    <div className="w-full h-full flex justify-between items-center mx-5 my-5 py-5">
+      <div className="flex flex-col justify-center space-y-2">
         <h2 className="text-lg text-neutral-800 font-medium mb-2">
-          Hi, my name is Harsh Keshari.
+          Hi, my name is <span className="font-semibold">Harsh Keshari</span>.
         </h2>
-        <h1 className="text-5xl text-neutral-900 font-bold">I Code.</h1>
+        <h1 className="text-5xl text-neutral-900 font-bold font-Rampart">I Code.</h1>
         <p className="text-lg text-neutral-600 font-medium mt-2">
-          I'm fullstack developer from India.
+          I'm a full-stack developer from India.
         </p>
         <div className="relative">
           <button className="bg-amber-300 border border-slate-950 shadow-sm shadow-black rounded-full w-1/2 py-2 transition ease-in-out delay-150 hover:-translate-x-1 hover:-translate-y-1 duration-300">
@@ -18,7 +19,9 @@ const Header = () => {
           <div className="bg-black rounded-full w-1/2 absolute inset-0 z-[-1]"></div>
         </div>
       </div>
-      <div>photo</div>
+      <div className="">
+        <Image src="/Img/hrshkshri.png" alt="Logo" width={500} height={500} />
+      </div>
     </div>
   );
 };
