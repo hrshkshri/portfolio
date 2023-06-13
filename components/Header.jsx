54ca47f023eb01aa085b220fe0f033f7795a5870
Harsh <harshkeshari100@gmail.com>
2023-06-13 17:09:31 +0530

    working on header section

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,8 +2,19 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      <div>name</div>
+    <div className="w-full h-full flex justify-between items-center mx-5 my-5 py-5 ">
+      <div className="flex flex-col space-y-2">
+        <h2 className="text-lg text-neutral-800 font-medium">
+          Hi, my name is Harsh Keshari.
+        </h2>
+        <h1 className="text-5xl text-neutral-900 font-bold">I Code.</h1>
+        <p className="text-lg text-neutral-800 font-medium">
+          I'm fullstack developer from India.
+        </p>
+        <button className="bg-amber-400 border border-slate-950 shadow-md shadow-black rounded-full w-1/2 py-2 transition ease-in-out delay-150 hover:-translate-x-1 hover:-translate-y-1 duration-300">
+          Say Hello
+        </button>
+      </div>
       <div>photo</div>
     </div>
   );
