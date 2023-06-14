dba0ba3c0f62abdc7704d59ac9ffb35685b6e6e5
Harsh <harshkeshari100@gmail.com>
2023-06-15 03:05:59 +0530

    checkpoint2

--- components/Home.jsx
+++ components/Home.jsx
@@ -7,37 +7,37 @@ const Home = () => {
   return (
     <Container activeRoute={"home"}>
       {/* <ParticleBackground /> */}
-      <div className="grid place-content-center" >
+      <div className="grid place-content-center h-full" >
         <div className="w-full h-full flex flex-col justify-center space-y-2 p-10 text-white relative">
-          <h2 className="text-xl text-neutral-400 font-medium font-Work">
-            Hi, I'm{" "}
-            <span className="text-2xl font-semibold text-amber-100 drop-shadow-md">
-              Harsh Keshari
-            </span>
-            .
-          </h2>
           <div className="flex space-x-1 items-center">
-            <h1 className="text-9xl text-amber-200 font-bold font-Rampart ">
-              I Code.
-            </h1>
+            <div className="flex flex-col">
+              <h2 className="text-4xl text-neutral-400 font-medium font-sans">
+                Hi, I'm{" "}
+                <span className="text-6xl font-bold text-amber-400 drop-shadow-lg shadow-neutral-100 font-serif">
+                  Harsh
+                </span>
+                .
+              </h2>
+              <h1 className="lg:text-9xl min-[320px]:max-md:text-8xl  text-amber-200 font-bold font-Rampart ">
+                I Code.
+              </h1>
+            </div>
             <Image
-              className="rounded-medium"
+              className="block min-[320px]:max-lg:hidden"
               src="/Img/2.svg"
               alt="Logo"
-              width={300}
-              height={300}
+              width={400}
+              height={400}
             />
           </div>
-          <p className="text-xl text-neutral-300 font-medium mt-2 font-Work">
-            A full-stack developer from India.
-          </p>
-          <div className="relative mt-2">
-            <button className="bg-[#ffb400] shadow-sm shadow-gray-50 rounded-full w-1/2 py-2 transition ease-in-out delay-150 hover:-translate-x-1 hover:-translate-y-1 duration-300">
-              <p className="text-md text-neutral-900 font-semibold">
-                Say Hello
-              </p>
-            </button>
-            <div className="bg-gray-100 rounded-full w-1/2 absolute inset-0 z-[-1]"></div>
+          <div className="min-[320px]:max-md:flex min-[320px]:max-md:justify-center min-[320px]:max-md:items-center">
+            <Image
+              className="lg:hidden block"
+              src="/Img/2.svg"
+              alt="Logo"
+              width={400}
+              height={400}
+            />
           </div>
         </div>
       </div>
