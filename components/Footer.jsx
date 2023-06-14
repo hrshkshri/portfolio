a7ef9345bbaf385af0100ea88fddf50e749dad6d
Harsh <harshkeshari100@gmail.com>
2023-06-15 04:31:06 +0530

    added another cool section in home route

--- components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="flex justify-center items-center">
-      Designed & Built by Harsh Keshari
-    </footer>
-  );
-}
--- components/Home.jsx
+++ components/Home.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import Image from "next/image";
 import Container from "./shared/Container";
 import ParticleBackground from "./shared/Particle/ParticleBackground";
+import Link from "next/link";
+import { MdKeyboardDoubleArrowRight } from "react-icons/md";
+import { BsFileEarmarkText } from "react-icons/bs";
+import { AiFillGithub } from "react-icons/ai";
 
 const Home = () => {
   return (
     <Container activeRoute={"home"}>
-      {/* <ParticleBackground /> */}
-      <div className="grid place-content-center h-full" >
+      <ParticleBackground />
+      <div className="grid place-content-center min-h-screen" >
         <div className="w-full h-full flex flex-col justify-center space-y-2 p-10 text-white relative">
           <div className="flex space-x-1 items-center">
             <div className="flex flex-col">
@@ -18,7 +22,7 @@ const Home = () => {
                 </span>
                 .
               </h2>
-              <h1 className="lg:text-9xl min-[320px]:max-md:text-8xl  text-amber-200 font-bold font-Rampart ">
+              <h1 className="lg:text-9xl min-[320px]:max-lg:text-8xl  text-amber-200 font-bold font-Rampart ">
                 I Code.
               </h1>
             </div>
@@ -40,6 +44,36 @@ const Home = () => {
             />
           </div>
         </div>
+      </div>
+      <div className="grid grid-cols-4 gap-4 text-neutral-200 m-4">
+        <Link href="/about" className="col-span-2 bg-neutral-900 rounded-md px-5 py-5 flex flex-col space-y-3">
+          <div>
+            <Image
+              className="bg-amber-500 rounded-full"
+              src="/Img/Harsh.png"
+              alt="Logo"
+              width={100}
+              height={100}
+            />
+          </div>
+          <div className="font-bold text-2xl font-serif">
+            <span className="text-amber-500 font-bold">Harsh</span><br /><span className="text-white">Keshari</span>
+          </div>
+          <div className="flex justify-between items-center">
+            <p className="font-sans text-lg font-semibold">
+              Full Stack Developer 💻 from India
+            </p>
+            <MdKeyboardDoubleArrowRight className="w-8 h-8 hover:text-amber-500" />
+          </div>
+        </Link>
+        <Link href="/resume" className="bg-neutral-900 rounded-md px-5 py-5 flex flex-col justify-between space-y-3">
+          <BsFileEarmarkText className="w-10 h-10 hover:text-amber-500" />
+          <p className="font-bold text-2xl font-sans">Resume</p>
+        </Link>
+        <Link href="https://github.com/hrshkshri" className="bg-neutral-900 rounded-md px-5 py-5 flex flex-col justify-between space-y-3">
+          <AiFillGithub className="w-10 h-10 hover:text-amber-500" />
+          <p className="font-bold text-2xl font-sans">Github</p>
+        </Link>
       </div>
     </Container>
   );
