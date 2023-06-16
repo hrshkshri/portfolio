84b18048dbfbd027665effa618c5355ebeee9584
Harsh <harshkeshari100@gmail.com>
2023-06-17 05:19:41 +0530

    UI of contact form

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -10,7 +10,10 @@ const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_tddh4it', 'template_fveb06v', form.current, 'zEQQStgESz901eTav')
+    const SERVICE_ID = process.env.SERVICE_ID;
+    const TEMPLATE_ID = process.env.TEMPLATE_ID;
+    const PUBLIC_KEY = process.env.PUBLIC_KEY;
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY)
       .then((result) => {
         console.log(result.text);
       }, (error) => {
@@ -21,11 +24,11 @@ const Contact = () => {
   return (
     <Container activeRoute={"contact"}>
       {!isSubmitted ? (
-        <div className='flex space-x-3 justify-between m-5'>
-          <div className='text-neutral-300 flex flex-cols space-y-3'>
-            <h1 className='text-4xl font-medium font-Work p-5 m-5 items-center m-2'>Get in Touch</h1>
-            <h3 className="font-sans text-xl font-semibold m-1">I'd love to hear your ideas and thoughts!</h3>
-            <div className='flex flex-col space-y-1 m-1 p-1'>
+        <div className='flex justify-between m-5'>
+          <div className='text-neutral-300 flex flex-col space-y-3'>
+            <h1 className='text-4xl font-bold font-Work py-5'>Get in Touch</h1>
+            <h3 className="font-sans text-xl font-semibold">I'd love to hear your ideas and thoughts!</h3>
+            <div className='flex flex-col space-y-1'>
               <p>Alternatively you can email me at:</p><br />
               <span className="text-amber-500 font-bold">harshkeshari100@gmail.com</span>
             </div>
@@ -33,12 +36,12 @@ const Contact = () => {
           <div className="bg-neutral-900 rounded-md px-5 py-5 hover:drop-shadow-[0_10px_8px_rgba(231,229,228,0.04)]">
             <form className="flex flex-col space-y-4 bg-neutral-900 rounded-md p-5 m-2" ref={form} onSubmit={sendEmail}>
               <label className="font-sans text-lg font-semibold text-neutral-300">Your Name</label>
-              <input className="px-4 py-2 rounded-md bg-neutral-500 text-gray-900" type="text" name="user_name" />
+              <input className="px-4 py-2 rounded-md bg-neutral-500 text-gray-100" type="text" name="user_name" placeholder="Enter your name" />
               <label className="font-sans text-lg font-semibold text-neutral-300">Your Email</label>
-              <input className="px-4 py-2 rounded-md bg-neutral-500 text-gray-900" type="email" name="user_email" />
+              <input className="px-4 py-2 rounded-md bg-neutral-500 text-gray-100" type="email" name="user_email" placeholder="Enter your email" />
               <label className="font-sans text-lg font-semibold text-neutral-300">Message</label>
-              <textarea name="message" />
-              <button className="bg-amber-500 hover:bg-amber-600 text-white px-4 py-2 rounded-md" type="submit" onClick={setIsSubmitted(true)}>Send </button>
+              <textarea className="px-4 py-2 rounded-md bg-neutral-500 text-gray-100" name="message" placeholder="Enter your message" />
+              <button className="bg-amber-500 hover:bg-amber-600 text-black px-4 py-2 rounded-md" type="submit" onClick={() => setIsSubmitted(true)}>Send</button>
             </form>
           </div>
         </div>
