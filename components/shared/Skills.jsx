2a0be608279ac94ca2ad0393ff3a9ffcdab77243
hrshkshri <harshkeshari100@gmail.com>
2024-06-09 12:25:38 +0530

    changed the skills UI

--- components/shared/Skills.jsx
+++ components/shared/Skills.jsx
@@ -1,91 +1,56 @@
 import React from "react";
 import { BsGit } from "react-icons/bs";
-import { AiFillHtml5 } from "react-icons/ai";
-import { TbBrandCpp, TbBrandNextjs } from "react-icons/tb";
-import { SiJavascript, SiTypescript, SiTailwindcss, SiExpress, SiPostman, SiMongodb, SiFirebase } from "react-icons/si";
-import { IoLogoCss3 } from "react-icons/io";
+import { TbBrandCpp, TbBrandNextjs, TbSql } from "react-icons/tb";
+import { SiJavascript, SiTypescript, SiTailwindcss, SiExpress, SiPostman, SiMongodb, SiFirebase, SiGo, SiAngular, SiPostgresql, SiMysql } from "react-icons/si";
 import { FaReact, FaNodeJs, FaLinux } from "react-icons/fa";
 const Skills = () => {
     return (
         <div className="text-neutral-50" >
             <h1 className="text-4xl text-neutral-300 font-medium font-Work p-5 m-5 items-center text-center">Skills I Possess</h1>
-            <div className="grid lg:grid-cols-5 sm:max-lg:grid-cols-3 min-[320px]:max-sm:grid-cols-2 gap-4 grid-flow-row auto-rows-max m-5">
-                <div className="flex flex-col justify-between items-center hover:drop-shadow-[0_10px_8px_rgba(231,229,228,0.04)]">
-                    <div className="p-5 bg-neutral-900 rounded-lg">
-                        <TbBrandCpp className="w-20 h-20 text-amber-300" />
+
+            <div className="bg-neutral-900 rounded-md px-5 py-5 flex flex-col">
+                <div className="flex flex-wrap space-x-3 items-center">
+                    <div className="font-bold text-xl font-serif">
+                        Languages:
                     </div>
+                    <TbBrandCpp className="w-5 h-5 text-amber-300" />
+                    <SiJavascript className="w-5 h-5 text-amber-300" />
+                    <SiTypescript className="w-5 h-5 text-amber-300" />
+                    <SiGo className="w-8 h-8 text-amber-300" />
+                    <TbSql className="w-7 h-7 text-amber-300" />
                 </div>
-                <div className="flex flex-col justify-between items-center hover:drop-shadow-[0_10px_8px_rgba(231,229,228,0.04)]">
-                    <div className="p-5 bg-neutral-900 rounded-lg">
-                        <AiFillHtml5 className="w-20 h-20 text-amber-300" />
+
+                <div className="flex flex-wrap space-x-3 items-center">
+                    <div className="font-bold text-xl font-serif">
+                        Libraries/Frameworks:
                     </div>
+                    <SiTailwindcss className="w-5 h-5 text-amber-300" />
+                    <FaReact className="w-5 h-5 text-amber-300" />
+                    <SiAngular className="w-5 h-5 text-amber-300" />
+                    <TbBrandNextjs className="w-5 h-5 text-amber-300" />
+                    <FaNodeJs className="w-5 h-5 text-amber-300" />
+                    <SiExpress className="w-5 h-5 text-amber-300" />
                 </div>
-                <div className="flex flex-col justify-between items-center hover:drop-shadow-[0_10px_8px_rgba(231,229,228,0.04)]">
-                    <div className="p-5 bg-neutral-900 rounded-lg">
-                        <IoLogoCss3 className="w-20 h-20 text-amber-300" />
+
+                <div className="flex flex-wrap space-x-3 items-center">
+                    <div className="font-bold text-xl font-serif">
+                        Databases:
                     </div>
+                    <SiMongodb className="w-5 h-5 text-amber-300" />
+                    <SiPostgresql className="w-5 h-5 text-amber-300" />
+                    <SiMysql className="w-7 h-7 text-amber-300" />
                 </div>
-                <div className="flex flex-col justify-between items-center hover:drop-shadow-[0_10px_8px_rgba(231,229,228,0.04)]">
-                    <div className="p-5 bg-neutral-900 rounded-lg">
-                        <SiJavascript className="w-20 h-20 text-amber-300" />
+
+                <div className="flex flex-wrap space-x-3 items-center">
+                    <div className="font-bold text-xl font-serif">
+                        Tools/Technologies:
                     </div>
+                    <SiPostman className="w-5 h-5 text-amber-300" />
+                    <SiFirebase className="w-5 h-5 text-amber-300" />
+                    <BsGit className="w-5 h-5 text-amber-300" />
+                    <FaLinux className="w-5 h-5 text-amber-300" />
                 </div>
-                <div className="flex flex-col justify-between items-center hover:drop-shadow-[0_10px_8px_rgba(231,229,228,0.04)]">
-                    <div className="p-5 bg-neutral-900 rounded-lg">
-                        <SiTypescript className="w-20 h-20 text-amber-300" />
-                    </div>
-                </div>
-                <div className="flex flex-col justify-between items-center hover:drop-shadow-[0_10px_8px_rgba(231,229,228,0.04)]">
-                    <div className="p-5 bg-neutral-900 rounded-lg">
-                        <FaReact className="w-20 h-20 text-amber-300" />
-                    </div>
-                </div>
-                <div className="flex flex-col justify-between items-center hover:drop-shadow-[0_10px_8px_rgba(231,229,228,0.04)]">
-                    <div className="p-5 bg-neutral-900 rounded-lg">
-                        <TbBrandNextjs className="w-20 h-20 text-amber-300" />
-                    </div>
-                </div>
-                <div className="flex flex-col justify-between items-center hover:drop-shadow-[0_10px_8px_rgba(231,229,228,0.04)]">
-                    <div className="p-5 bg-neutral-900 rounded-lg">
-                        <SiTailwindcss className="w-20 h-20 text-amber-300" />
-                    </div>
-                </div>
-                <div className="flex flex-col justify-between items-center hover:drop-shadow-[0_10px_8px_rgba(231,229,228,0.04)]">
-                    <div className="p-5 bg-neutral-900 rounded-lg">
-                        <FaNodeJs className="w-20 h-20 text-amber-300" />
-                    </div>
-                </div>
-                <div className="flex flex-col justify-between items-center hover:drop-shadow-[0_10px_8px_rgba(231,229,228,0.04)]">
-                    <div className="p-5 bg-neutral-900 rounded-lg">
-                        <SiExpress className="w-20 h-20 text-amber-300" />
-                    </div>
-                </div>
-                <div className="flex flex-col justify-between items-center hover:drop-shadow-[0_10px_8px_rgba(231,229,228,0.04)]">
-                    <div className="p-5 bg-neutral-900 rounded-lg">
-                        <SiPostman className="w-20 h-20 text-amber-300" />
-                    </div>
-                </div>
-                <div className="flex flex-col justify-between items-center hover:drop-shadow-[0_10px_8px_rgba(231,229,228,0.04)]">
-                    <div className="p-5 bg-neutral-900 rounded-lg">
-                        <SiMongodb className="w-20 h-20 text-amber-300" />
-                    </div>
-                </div>
-                <div className="flex flex-col justify-between items-center hover:drop-shadow-[0_10px_8px_rgba(231,229,228,0.04)]">
-                    <div className="p-5 bg-neutral-900 rounded-lg">
-                        <SiFirebase className="w-20 h-20 text-amber-300" />
-                    </div>
-                </div>
-                <div className="flex flex-col justify-between items-center hover:drop-shadow-[0_10px_8px_rgba(231,229,228,0.04)]">
-                    <div className="p-5 bg-neutral-900 rounded-lg">
-                        <BsGit className="w-20 h-20 text-amber-300" />
-                    </div>
-                </div>
-                <div className="flex flex-col justify-between items-center hover:drop-shadow-[0_10px_8px_rgba(231,229,228,0.04)]">
-                    <div className="p-5 bg-neutral-900 rounded-lg">
-                        <FaLinux className="w-20 h-20 text-amber-300" />
-                    </div>
-                </div>
-                {/* add more skills here */}
+
             </div>
         </div>
     )
