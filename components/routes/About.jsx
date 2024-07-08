13f4d5abaaf72a2a7710cf859b518506da43454c
hrshkshri <harshkeshari100@gmail.com>
2024-07-08 18:14:37 +0530

    final project ui

--- components/routes/About.jsx
+++ components/routes/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
                     </span>
                   </Link>
                 </div>
-                <ul className="mt-2 text-sm list-disc list-inside">
+                <ul className="mt-2 text-sm list-disc list-inside text-neutral-500">
                   <li>Developed a new feature for the Amazon Prime Video app that allows users to create and manage watch parties with friends and family.</li>
                   <li>The feature was built using React, Redux, and Node.js, and was launched to millions of users in the US and UK.</li>
                 </ul>
@@ -63,14 +63,14 @@ const About = () => {
           <div className="mt-5">
             <div className="border-l-2 pl-1 hover:border-amber-500">
               <div className="group relative mb-1 p-2">
-              <div className="flex flex-col justify-normal">
+                <div className="flex flex-col justify-normal">
                   <p className="font-semibold text-amber-500">The Oppia Foundations</p>
 
                   <div className="text-sm italic space-x-2 flex items-center">
                     <Link href="https://www.livepreview.com" className="transition ease-in-out duration-200 transform hover:-translate-y-0.5 hover:-translate-x-0.5 cursor-pointer">
-                      <span className="flex items-center">
+                      <span className="flex items-center text-neutral-500">
                         @oppia
-                        <MdArrowOutward className="w-4 h-4 text-neutral-100 ml-1" />
+                        <MdArrowOutward className="w-4 h-4 ml-1 text-neutral-500" />
                       </span>
                     </Link>
                   </div>
@@ -81,22 +81,22 @@ const About = () => {
 
                   <div className="text-sm italic space-x-2 flex items-center">
                     <Link href="https://www.livepreview.com" className="transition ease-in-out duration-200 transform hover:-translate-y-0.5 hover:-translate-x-0.5 cursor-pointer">
-                      <span className="flex items-center">
+                      <span className="flex items-center text-neutral-500">
                         @tailcallhq
-                        <MdArrowOutward className="w-4 h-4 text-neutral-100 ml-1" />
+                        <MdArrowOutward className="w-4 h-4 ml-1 text-neutral-500" />
                       </span>
                     </Link>
                   </div>
                 </div>
-                
+
                 <div className="flex flex-col justify-normal">
                   <p className="font-semibold text-amber-500">CircuitVerse</p>
 
                   <div className="text-sm italic space-x-2 flex items-center">
                     <Link href="https://www.livepreview.com" className="transition ease-in-out duration-200 transform hover:-translate-y-0.5 hover:-translate-x-0.5 cursor-pointer">
-                      <span className="flex items-center">
+                      <span className="flex items-center text-neutral-500">
                         @CircuitVerse
-                        <MdArrowOutward className="w-4 h-4 text-neutral-100 ml-1" />
+                        <MdArrowOutward className="w-4 h-4 ml-1  text-neutral-500" />
                       </span>
                     </Link>
                   </div>
@@ -130,14 +130,18 @@ const About = () => {
                     </span>
                   </Link>
                 </div>
-                <ul className="mt-4 text-sm list-disc list-inside space-y-1">
-                  <li>Developed a new feature for the Amazon Prime Video app that allows users to create and manage watch parties with friends and family.</li>
-                  <li>The feature was built using React, Redux, and Node.js, and was launched to millions of users in the US and UK.</li>
-                </ul>
-                <div className="mt-4 flex items-center space-x-1">
-                  <p className="font-semibold">Tech Stack</p>
-                  <p className="text-sm">React, Redux, Node.js, Express, MongoDB, Tailwind CSS</p>
+                <div className="text-neutral-500">
+
+                  <ul className="mt-4 text-sm list-disc list-inside space-y-1">
+                    <li>Developed a new feature for the Amazon Prime Video app that allows users to create and manage watch parties with friends and family.</li>
+                    <li>The feature was built using React, Redux, and Node.js, and was launched to millions of users in the US and UK.</li>
+                  </ul>
+                  <div className="mt-4 flex items-center space-x-1">
+                    <p className="font-semibold">Tech Stack:</p>
+                    <p className="italic">React, Redux, Node.js, Express, MongoDB, Tailwind CSS</p>
+                  </div>
                 </div>
+
               </div>
             </div>
           </div>
